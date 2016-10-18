79db7e7cc6c4cad4a9b2ce2920927f95f35f3bce
Gilang Chandrasa <gilang.chandrasa@gmail.com>
2016-10-18 14:30:02 +0700

    Switch to sqlite for circleci

--- bookworm/settings/circleci.py
+++ bookworm/settings/circleci.py
@@ -1,10 +1,9 @@
+import os
 from .base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'circle_test',
-        'USER': 'ubuntu',
-        'PASSWORD': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
